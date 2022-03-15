5ba53fcfcf1a1bf749221e3e06962ff8762fe960
Matvey Gladkikh <matvey@copters.ru>
2022-03-15 13:18:45 +0300

    Update sitebrush.sql

--- sitebrush.sql
+++ sitebrush.sql
@@ -1,28 +1,7 @@
--- MySQL dump 10.16  Distrib 10.1.48-MariaDB, for debian-linux-gnu (x86_64)
---
--- Host: localhost    Database: sitebrush
--- ------------------------------------------------------
--- Server version	10.1.48-MariaDB-0+deb9u2
+CREATE DATABASE IF NOT EXISTS `sitebrush`;
+USE `sitebrush`;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `domain`
---
-
-DROP TABLE IF EXISTS `domain`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `domain` (
+CREATE TABLE IF NOT EXISTS `domain` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` text NOT NULL,
   `dnszonedata` text NOT NULL,
@@ -34,14 +13,7 @@ CREATE TABLE `domain` (
 ) ENGINE=InnoDB AUTO_INCREMENT=92 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `domain_language`
---
-
-DROP TABLE IF EXISTS `domain_language`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `domain_language` (
+CREATE TABLE IF NOT EXISTS `domain_language` (
   `domain_id` int(11) NOT NULL,
   `language_id` int(11) NOT NULL,
   PRIMARY KEY (`domain_id`,`language_id`),
@@ -51,14 +23,7 @@ CREATE TABLE `domain_language` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `group`
---
-
-DROP TABLE IF EXISTS `group`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `group` (
+CREATE TABLE IF NOT EXISTS `group` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `owner_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
@@ -71,14 +36,7 @@ CREATE TABLE `group` (
 ) ENGINE=InnoDB AUTO_INCREMENT=81325 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `group_media`
---
-
-DROP TABLE IF EXISTS `group_media`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `group_media` (
+CREATE TABLE IF NOT EXISTS `group_media` (
   `group_id` int(11) NOT NULL,
   `media_id` int(11) NOT NULL,
   PRIMARY KEY (`group_id`,`media_id`),
@@ -88,14 +46,7 @@ CREATE TABLE `group_media` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `group_message`
---
-
-DROP TABLE IF EXISTS `group_message`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `group_message` (
+CREATE TABLE IF NOT EXISTS `group_message` (
   `group_id` int(11) NOT NULL,
   `message_id` int(11) NOT NULL,
   PRIMARY KEY (`group_id`,`message_id`),
@@ -105,14 +56,7 @@ CREATE TABLE `group_message` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `group_post`
---
-
-DROP TABLE IF EXISTS `group_post`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `group_post` (
+CREATE TABLE IF NOT EXISTS `group_post` (
   `group_id` int(11) NOT NULL,
   `post_id` int(11) NOT NULL,
   PRIMARY KEY (`group_id`,`post_id`),
@@ -122,14 +66,7 @@ CREATE TABLE `group_post` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `invite`
---
-
-DROP TABLE IF EXISTS `invite`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `invite` (
+CREATE TABLE IF NOT EXISTS `invite` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `hash` varchar(255) NOT NULL,
   `status` varchar(255) DEFAULT NULL,
@@ -138,14 +75,8 @@ CREATE TABLE `invite` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `language`
---
 
-DROP TABLE IF EXISTS `language`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `language` (
+CREATE TABLE IF NOT EXISTS `language` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `code` varchar(255) NOT NULL,
   `revision` int(11) NOT NULL,
@@ -156,14 +87,7 @@ CREATE TABLE `language` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `media`
---
-
-DROP TABLE IF EXISTS `media`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `media` (
+CREATE TABLE IF NOT EXISTS `media` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `type` varchar(255) NOT NULL,
   `hash` varchar(255) NOT NULL,
@@ -185,14 +109,7 @@ CREATE TABLE `media` (
 ) ENGINE=InnoDB AUTO_INCREMENT=55214 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `message`
---
-
-DROP TABLE IF EXISTS `message`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `message` (
+CREATE TABLE IF NOT EXISTS `message` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `to` int(11) DEFAULT NULL,
   `from` int(11) DEFAULT NULL,
@@ -209,14 +126,7 @@ CREATE TABLE `message` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `message_media`
---
-
-DROP TABLE IF EXISTS `message_media`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `message_media` (
+CREATE TABLE IF NOT EXISTS `message_media` (
   `message_id` int(11) NOT NULL,
   `media_id` int(11) NOT NULL,
   PRIMARY KEY (`message_id`,`media_id`),
@@ -226,14 +136,7 @@ CREATE TABLE `message_media` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `post`
---
-
-DROP TABLE IF EXISTS `post`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `post` (
+CREATE TABLE IF NOT EXISTS `post` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `ownerid` int(11) NOT NULL,
   `deleterid` int(11) NOT NULL,
@@ -252,14 +155,7 @@ CREATE TABLE `post` (
 ) ENGINE=InnoDB AUTO_INCREMENT=19706 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `post_media`
---
-
-DROP TABLE IF EXISTS `post_media`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `post_media` (
+CREATE TABLE IF NOT EXISTS `post_media` (
   `post_id` int(11) NOT NULL,
   `media_id` int(11) NOT NULL,
   PRIMARY KEY (`post_id`,`media_id`),
@@ -269,14 +165,7 @@ CREATE TABLE `post_media` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `post_message`
---
-
-DROP TABLE IF EXISTS `post_message`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `post_message` (
+CREATE TABLE IF NOT EXISTS `post_message` (
   `post_id` int(11) NOT NULL,
   `message_id` int(11) NOT NULL,
   PRIMARY KEY (`post_id`,`message_id`),
@@ -286,14 +175,7 @@ CREATE TABLE `post_message` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `post_template`
---
-
-DROP TABLE IF EXISTS `post_template`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `post_template` (
+CREATE TABLE IF NOT EXISTS `post_template` (
   `post_id` int(11) NOT NULL,
   `template_id` int(11) NOT NULL,
   PRIMARY KEY (`post_id`,`template_id`),
@@ -303,26 +185,13 @@ CREATE TABLE `post_template` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `propel_migration`
---
 
-DROP TABLE IF EXISTS `propel_migration`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `propel_migration` (
+CREATE TABLE IF NOT EXISTS `propel_migration` (
   `version` int(11) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `template`
---
-
-DROP TABLE IF EXISTS `template`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `template` (
+CREATE TABLE IF NOT EXISTS `template` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` text NOT NULL,
   `data` longtext NOT NULL,
@@ -332,14 +201,8 @@ CREATE TABLE `template` (
 ) ENGINE=InnoDB AUTO_INCREMENT=88 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `uri`
---
 
-DROP TABLE IF EXISTS `uri`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `uri` (
+CREATE TABLE IF NOT EXISTS `uri` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `olduri` varchar(255) NOT NULL,
   `newuri` varchar(255) NOT NULL,
@@ -350,14 +213,7 @@ CREATE TABLE `uri` (
 ) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `user`
---
-
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user` (
+CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `sessionid` varchar(255) NOT NULL,
   `oldid` int(11) NOT NULL,
@@ -394,14 +250,7 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB AUTO_INCREMENT=87 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `user_group`
---
-
-DROP TABLE IF EXISTS `user_group`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user_group` (
+CREATE TABLE IF NOT EXISTS `user_group` (
   `user_id` int(11) NOT NULL,
   `group_id` int(11) NOT NULL,
   PRIMARY KEY (`user_id`,`group_id`),
@@ -411,14 +260,8 @@ CREATE TABLE `user_group` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `user_invite`
---
 
-DROP TABLE IF EXISTS `user_invite`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user_invite` (
+CREATE TABLE IF NOT EXISTS `user_invite` (
   `user_id` int(11) NOT NULL,
   `invite_id` int(11) NOT NULL,
   PRIMARY KEY (`user_id`,`invite_id`),
@@ -428,14 +271,7 @@ CREATE TABLE `user_invite` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `user_media`
---
-
-DROP TABLE IF EXISTS `user_media`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user_media` (
+CREATE TABLE IF NOT EXISTS `user_media` (
   `user_id` int(11) NOT NULL,
   `media_id` int(11) NOT NULL,
   PRIMARY KEY (`user_id`,`media_id`),
@@ -445,14 +281,7 @@ CREATE TABLE `user_media` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `user_message`
---
-
-DROP TABLE IF EXISTS `user_message`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user_message` (
+CREATE TABLE IF NOT EXISTS `user_message` (
   `user_id` int(11) NOT NULL,
   `message_id` int(11) NOT NULL,
   PRIMARY KEY (`user_id`,`message_id`),
@@ -462,14 +291,7 @@ CREATE TABLE `user_message` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `user_post`
---
-
-DROP TABLE IF EXISTS `user_post`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user_post` (
+CREATE TABLE IF NOT EXISTS `user_post` (
   `user_id` int(11) NOT NULL,
   `post_id` int(11) NOT NULL,
   PRIMARY KEY (`user_id`,`post_id`),
