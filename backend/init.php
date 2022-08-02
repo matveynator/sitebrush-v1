33ad1990e63ed8139a330714734ff2697243d402
root <root@sitebrush.com>
2022-08-02 10:14:44 +0300

    portnov.kmv.ru domain exceprtion

--- backend/init.php
+++ backend/init.php
@@ -16,14 +16,22 @@ if (PHP_SAPI === 'cli') {
 } else {
 	ini_set("memory_limit", $configuration['upload_max_filesize']);
 	$subdomain_name = implode(array_slice(explode('.', $_SERVER['HTTP_HOST']), - 3, 1, TRUE));
-	if (((count(explode('.', $_SERVER['HTTP_HOST'])))>2) and (isset($subdomain_name)) and ($subdomain_name != "") and ($subdomain_name != "www")) {
-		$configuration['domain'] = implode('.', array_slice(explode('.', $_SERVER['HTTP_HOST']), - 2));
-		//echo "<br>1<pre>";
+
+	//исключение домен вадима портнова
+	if ($_SERVER['HTTP_HOST'] == "portnov.kmv.ru") {
+		$configuration['domain'] = "portnov.kmv.ru";
+		//echo "<br>0<pre>";
 		//echo $configuration['domain'];
 	} else {
-		//echo "2<br><pre>";
-		$configuration['domain'] = implode('.', array_slice(explode('.', $_SERVER['HTTP_HOST']), - 2));
-		//echo $configuration['domain'];
+		if (((count(explode('.', $_SERVER['HTTP_HOST'])))>2) and (isset($subdomain_name)) and ($subdomain_name != "") and ($subdomain_name != "www")) {
+			$configuration['domain'] = implode('.', array_slice(explode('.', $_SERVER['HTTP_HOST']), - 2));
+			//echo "<br>1<pre>";
+			//echo $configuration['domain'];
+		} else {
+			$configuration['domain'] = implode('.', array_slice(explode('.', $_SERVER['HTTP_HOST']), - 2));
+			//echo "2<br><pre>";
+			//echo $configuration['domain'];
+		}
 	}
 }
 
@@ -148,8 +156,8 @@ if (PHP_SAPI !== 'cli') {
 		and set http://domain.com/>>$url_path[1]<</>>$url_path[2]<</>>$url_path[3]<</ etc...
 	 */
 
-	//redirect any >2 level domain to 2 level domain
-	if (((count(explode('.', $_SERVER['HTTP_HOST'])))>2) and (isset($subdomain_name)) and ($subdomain_name != "") and ($subdomain_name != "www")) {
+	//redirect any >2 level domain to 2 level domain except portnov.kmv.ru
+	if (((count(explode('.', $_SERVER['HTTP_HOST'])))>2) and (isset($subdomain_name)) and ($subdomain_name != "") and ($subdomain_name != "www") and ($configuration['domain'] != "portnov.kmv.ru")) {
 		$configuration['domain'] = implode('.', array_slice(explode('.', $_SERVER['HTTP_HOST']), - 2));
 		if ($_SERVER['HTTP_SCHEME'] == 'https') {
 			Jump("https://${configuration['domain']}{$_SERVER['REQUEST_URI']}");
