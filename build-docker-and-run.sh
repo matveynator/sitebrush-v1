6a0d5601685407ec8acace0ca8233908f8b21767
root <root@static.87.211.4.46.clients.your-server.de>
2024-03-15 21:48:30 +0300

    docker

--- build-docker-and-run.sh
+++ build-docker-and-run.sh
@@ -1,16 +1,16 @@
+#!/bin/bash
+
 docker stop sitebrush
 docker rm sitebrush
 docker rmi sitebrush
-rm -rf /backup/sitebrush
-docker build -t sitebrush .
+docker build -t sitebrush-v1 .
 #docker run -d -p 80:80 -p 4443:443 -v "/etc/timezone:/etc/timezone:ro" -v "/etc/localtime:/etc/localtime:ro" -v /backup/sitebrush/db/conf.d:/etc/mysql/conf.d -v /backup/sitebrush/db/mysql:/var/lib/mysql -v /backup/sitebrush/data:/opt/sitebrush.com  --restart=unless-stopped --name=sitebrush sitebrush
 
 #docker run -d -p 80:80 -p 443:443 -v "/etc/timezone:/etc/timezone:ro" -v "/etc/localtime:/etc/localtime:ro" -v /backup/sitebrush/data:/opt/sitebrush.com  --restart=unless-stopped --name=sitebrush sitebrush
 
-docker run -d -p 80:80 -p 443:443 -v "/etc/timezone:/etc/timezone:ro" -v "/etc/localtime:/etc/localtime:ro" --restart=unless-stopped --name=sitebrush sitebrush
 
-sleep 2
-docker logs sitebrush
-sleep 2
-docker exec -it sitebrush bash
-echo "ok"
+docker tag sitebrush-v1:latest matveynator/sitebrush-v1:latest 
+
+docker run -d -p 80:80 -p 443:443 -v "/etc/timezone:/etc/timezone:ro" -v "/etc/localtime:/etc/localtime:ro" --restart=unless-stopped --name=sitebrush matveynator/sitebrush-v1:latest
+
+docker push matveynator/sitebrush-v1:latest
